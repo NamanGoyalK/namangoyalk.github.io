938f880e48ebec7fb5c4503b8e56641d82328615
Naman Goyal <namangoyaltms@outlook.com>
2025-04-12 21:15:35 +0530

    Update build configuration and resources for deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "514836777"
+    serviceWorkerVersion: "667711073"
   }
 });
